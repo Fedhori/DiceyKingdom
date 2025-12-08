a4d8d6f5c84aa6d25506990086922df04db26a3c
김병무 <edhori990611@gmail.com>
2025-12-08 17:30:30 +0900

    볼 툴팁 추가

--- Assets/Scripts/Ball/BallDeckView.cs
+++ Assets/Scripts/Ball/BallDeckView.cs
@@ -1,3 +1,4 @@
+using Data;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,10 +7,18 @@ public class BallDeckView : MonoBehaviour
 {
     [SerializeField] private Image ballIcon;
     [SerializeField] private TMP_Text ballCount;
+    [SerializeField] private BallUITooltipTarget ballUITooltipTarget;
 
     public void UpdateBallDeckView(string ballId, int count)
     {
         ballIcon.sprite = SpriteCache.GetBallSprite(ballId);
         ballCount.text = count.ToString();
+
+        BallRepository.TryGet(ballId, out BallDto dto);
+        if (dto == null)
+            return;
+
+        var ballInstance = new BallInstance(dto);
+        ballUITooltipTarget.Bind(ballInstance);
     }
 }
