abca79e1e3cf2b663417e806dc7c2657abb33cd8
김병무 <edhori990611@gmail.com>
2025-12-09 16:27:42 +0900

    Revert "conflict 수정"
    This reverts commit 00b2a50cff69edfa3108242e3b4cec8a1b582cfa, reversing
    changes made to 1f814c2ed6bf750625be9dec27921c54526cf886.

--- Assets/Scripts/Data/GameConfig.cs
+++ Assets/Scripts/Data/GameConfig.cs
@@ -6,9 +6,6 @@ public static class GameConfig
     public static string BasicPinId = "pin.basic";
     public static string BasicBallId = "ball.basic";
     public static int BaseIncome = 10;
-
-    public static int BallCostDivideByTwo = 100;
-    public static int BallCostDivideByThree = 150;
 }
 
 public static class LayerMaskUtil
