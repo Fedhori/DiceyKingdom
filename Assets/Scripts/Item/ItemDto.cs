36594107491dc7180e651c956be088b1420493a5
김병무 <edhori990611@gmail.com>
2025-12-31 14:38:58 +0900

    1.2

--- Assets/Scripts/Item/ItemDto.cs
+++ Assets/Scripts/Item/ItemDto.cs
@@ -1,11 +1,84 @@
 using System;
+using System.Collections.Generic;
+using GameStats;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Data
 {
+    public enum ItemTriggerType
+    {
+        Unknown = 0,
+        OnStageStart,
+        OnTick
+    }
+
+    public enum ItemConditionKind
+    {
+        Unknown = 0,
+        Always
+    }
+
+    public enum ItemEffectType
+    {
+        Unknown = 0,
+        ModifyStat,
+        AddCurrency
+    }
+
+    public enum ItemRarity
+    {
+        Unknown = 0,
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+    [Serializable]
+    public sealed class ItemConditionDto
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ItemConditionKind conditionKind;
+    }
+
+    [Serializable]
+    public sealed class ItemEffectDto
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ItemEffectType effectType;
+
+        public string statId;
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public StatOpKind effectMode;
+
+        public float value;
+        public bool temporary = true;
+    }
+
+    [Serializable]
+    public sealed class ItemRuleDto
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ItemTriggerType triggerType;
+
+        public ItemConditionDto condition;
+        public List<ItemEffectDto> effects;
+    }
+
     [Serializable]
     public sealed class ItemDto
     {
         public string id;
+        public bool isObject;
+        public int price;
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ItemRarity rarity = ItemRarity.Common;
+
+        public List<ItemRuleDto> rules;
         public float damageMultiplier = 1f;
         public float attackSpeed = 1f;
         public float bulletSize = 1f;
