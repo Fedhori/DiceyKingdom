e099b116065441f85f623022cdf22b80f5abb0a2
김병무 <edhori990611@gmail.com>
2026-01-06 12:12:27 +0900

    아이템 추가 - 돌격소총

--- Assets/Scripts/Data/GameConfig.cs
+++ Assets/Scripts/Data/GameConfig.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public static class GameConfig
 {
-    public static int BaseIncome = 10;
+    public static int BaseIncome = 5;
     public static int ItemSlotCount = 8;
 
-    // Player/Item base stats
+    // Player/Item base stats   
     public static float PlayerBaseMoveSpeed = 500f;
     public static float ItemBaseProjectileSize = 1f;
     public static float ItemBaseProjectileSpeed = 1000f;
